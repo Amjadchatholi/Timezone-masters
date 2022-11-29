e845b0c56a011a12fd5607b3e5d35b86e7bde13b
Amjad Chatholi <amjadchatholi@gmail.com>
2022-11-29 10:15:22 -0800

    portnumberchange

--- project_timezone/settings.py
+++ project_timezone/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-r$_mhe7@!7p=(gdv@j^kx)+00%u7tfdfede9=8wol&%@j^t0!r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ['web-production-aa25.up.railway.app']
+ALLOWED_HOSTS = ['web-production-aa25.up.railway.app', '127.0.0:1']
 
 
 # Application definition
@@ -100,7 +100,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'amjad12345',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '5612',
         
     }
 }
