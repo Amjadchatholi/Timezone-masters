3aaffea79950b2a722ac7f965cdf57efc73a327d
Amjad Chatholi <amjadchatholi@gmail.com>
2023-04-06 15:49:38 +0530

    last one deploying python anywhere

--- project_timezone/settings.py
+++ project_timezone/settings.py
@@ -100,28 +100,28 @@ AUTH_USER_MODEL = 'accounts.Account'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {    
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'timezone',
-        'PASSWORD': 'amjad8086',
-        'HOST': 'database-2.cspkw0mdzjni.ap-northeast-1.rds.amazonaws.com',
-        'PORT': '5432',
-        
-    }
-}
-
 # DATABASES = {
 #     'default': {    
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'test2',
-#         'USER': 'postgres',
-#         'PASSWORD': 'amjad12345',
-        
+#         'NAME': 'postgres',
+#         'USER': 'timezone',
+#         'PASSWORD': 'amjad8086',
+#         # 'HOST': 'database-2.cspkw0mdzjni.ap-northeast-1.rds.amazonaws.com',
+#         # 'PORT': '5432',
         
 #     }
 # }
+
+DATABASES = {
+    'default': {    
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test2',
+        'USER': 'postgres',
+        'PASSWORD': 'amjad12345',
+        
+        
+    }
+}
 
 # DATABASES = {
 #     'default': {
